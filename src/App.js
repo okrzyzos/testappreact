27bab5dc132dc4b601ee729042cc3ccab4e5327a
okrzyzos <okrzyzos@outlook.fr>
2023-09-06 12:23:05 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,77 +1,109 @@
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import './App.css';
+// import Test from './Test.js';
+// import Api from './Api.js';
+// import UserAPI from './UserAPI';
+// import DataGrid from './CustomDataGrid';
+// import Timeline from './Timeline.js';
+// import Cycle from './Cycle.js';
+// import Voyelles from './Voyelles.js';
+// import Filtre from './Filtre.js';
+
+// function App() {
+//   const [selectedCity, setSelectedCity] = useState('');
+//   const [sortedCities, setSortedCities] = useState([]);
+//   const [showCities, setShowCities] = useState(true);
+//   const cities = ['Lyon', 'Marseille', 'Paris', 'Lens'];
+
+//   function handleClick(city) {
+//     setSelectedCity(city);
+//   }
+
+//   function handleFilter() {
+//     const filteredCities = cities.filter(city => city.charAt(0).toLowerCase() === 'l');
+//     setSelectedCity(filteredCities.join(', '));
+//   }
+
+//   function handleReset() {
+//     setSelectedCity('');
+//   }
+
+//   function handleSort() {
+//     const sorted = [...cities].sort((a, b) => b.localeCompare(a));
+//     setSortedCities(sorted);
+//   }
+
+//   function handleToggleCities() {
+//     setShowCities(!showCities);
+//   }
+
+//   return (
+//     <div className="App">
+//       {/* <h1>Olivier krzyzostoniak</h1>
+//       {showCities && (
+//         <ul>
+//           {sortedCities.length > 0
+//             ? sortedCities.map((city) => (
+//                 <li key={city} onClick={() => handleClick(city)}>
+//                   {city}
+//                 </li>
+//               ))
+//             : cities.map((city) => (
+//                 <li key={city} onClick={() => handleClick(city)}>
+//                   {city}
+//                 </li>
+//               ))}
+//         </ul>
+//       )}
+//       <button onClick={handleFilter}>Afficher les villes commençant par "L"</button>
+//       <button onClick={handleReset}>Réinitialiser</button>
+//       <button onClick={handleSort}>Trier par ordre décroissant</button>
+//       <button onClick={handleToggleCities}>
+//         {showCities ? 'Masquer les villes' : 'Afficher les villes'}
+//       </button>
+//       <p>Selected city: {selectedCity}</p> */}
+//       <Filtre />
+//       {/* <Test /> */}
+//       {/* <Api /> */}
+//       {/* <UserAPI /> */}
+//       {/* <DataGrid /> */}
+//       {/* <Timeline /> */}
+//       {/* <Cycle /> */}
+//       {/* <Voyelles /> */}
+//     </div>
+//   );
+  
+// }
+
+// export default App;
+
 import React, { useState } from 'react';
 import './App.css';
-import Test from './Test.js';
-import Api from './Api.js';
-import UserAPI from './UserAPI';
-import DataGrid from './CustomDataGrid';
-import Timeline from './Timeline.js';
-import Cycle from './Cycle.js';
-import Voyelles from './Voyelles.js';
-import Filtre from './Filtre.js';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'; // Notez l'ajout de 'Routes' ici
+import Filtre from './Filtre.js'; // Importez le composant Filtre
+import NadalProfil from './NadalProfil';
+import AlcarazProfil from './AlcarazProfil';
 
 function App() {
-  const [selectedCity, setSelectedCity] = useState('');
-  const [sortedCities, setSortedCities] = useState([]);
-  const [showCities, setShowCities] = useState(true);
-  const cities = ['Lyon', 'Marseille', 'Paris', 'Lens'];
-
-  function handleClick(city) {
-    setSelectedCity(city);
-  }
-
-  function handleFilter() {
-    const filteredCities = cities.filter(city => city.charAt(0).toLowerCase() === 'l');
-    setSelectedCity(filteredCities.join(', '));
-  }
-
-  function handleReset() {
-    setSelectedCity('');
-  }
-
-  function handleSort() {
-    const sorted = [...cities].sort((a, b) => b.localeCompare(a));
-    setSortedCities(sorted);
-  }
-
-  function handleToggleCities() {
-    setShowCities(!showCities);
-  }
+  // Votre code existant ici
 
   return (
-    <div className="App">
-      {/* <h1>Olivier krzyzostoniak</h1>
-      {showCities && (
-        <ul>
-          {sortedCities.length > 0
-            ? sortedCities.map((city) => (
-                <li key={city} onClick={() => handleClick(city)}>
-                  {city}
-                </li>
-              ))
-            : cities.map((city) => (
-                <li key={city} onClick={() => handleClick(city)}>
-                  {city}
-                </li>
-              ))}
-        </ul>
-      )}
-      <button onClick={handleFilter}>Afficher les villes commençant par "L"</button>
-      <button onClick={handleReset}>Réinitialiser</button>
-      <button onClick={handleSort}>Trier par ordre décroissant</button>
-      <button onClick={handleToggleCities}>
-        {showCities ? 'Masquer les villes' : 'Afficher les villes'}
-      </button>
-      <p>Selected city: {selectedCity}</p> */}
-      <Filtre />
-      {/* <Test /> */}
-      {/* <Api /> */}
-      {/* <UserAPI /> */}
-      {/* <DataGrid /> */}
-      {/* <Timeline /> */}
-      {/* <Cycle /> */}
-      {/* <Voyelles /> */}
-    </div>
+    <Router>
+    
+
+      {/* Utilisez le composant Routes pour envelopper vos routes */}
+      <Routes>
+      <Route path="/"  element={<Filtre />} />
+        <Route path="/nadal"  element={<NadalProfil />} />
+        <Route path="/alcaraz"  element={<AlcarazProfil />} />
+      </Routes>
+
+    </Router>
+    
   );
 }
+
+// ... Le reste de votre code
 
 export default App;
---
+++ src/NadalProfil.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Filtre.css";
+
+
+function NadalProfil() {
+  return (
+    <div>
+      <div style={{textAlign:'center',textTransform:'uppercase'}}><strong>NadalProfil</strong></div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginTop: '30px' }}>
+        <img
+          src="https://static.cnews.fr/sites/default/files/styles/image_750_422/public/roland-garros_les_14_sacres_de_rafael_nadal_646615fc1736a.jpg"
+          alt="Rafael Nadal 2"
+          style={{ maxWidth: '100%', maxHeight: '400px',borderRadius:"20%" }} // Pour éviter le débordement de l'image
+        />
+        <div>
+          <p className='text-raccourci'>
+            On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions,
+            et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme
+            'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout
+            cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de
+            sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira
+            vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues
+            avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'œil,
+            voire des phrases embarrassantes).
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NadalProfil;
